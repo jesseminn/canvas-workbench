cc06febbbc903dfd5f418e1af5ff5fec863c69aa
Jesse Chen <jesseminn@gmail.com>
2020-12-31 00:25:29 +0800

    improvement: add `createGrid` util

--- src/lib/utils/number.utils.ts
+++ src/lib/utils/number.utils.ts
@@ -1,3 +1,5 @@
+import { Callback } from '../types';
+
 type Range = [number, number];
 
 export const isInRange = (range: Range, n: number, isInclusive?: boolean) => {
@@ -10,4 +12,22 @@ export const isInRange = (range: Range, n: number, isInclusive?: boolean) => {
     } else {
         return total > m && m > 0;
     }
+};
+
+export const createGrid = (width: number, height: number, columns: number, rows: number, callback?: Callback) => {
+    const col = width / columns;
+    const row = height / rows;
+
+    // The top-left position of every cell
+    const grid: [number, number][] = [];
+
+    for (let x = 0; x < width; x += col) {
+        for (let y = 0; y < height; y += row) {
+            grid.push([x, y]);
+            if (typeof callback === 'function') {
+                callback(x, y, col, row);
+            }
+        }
+    }
+    return { grid, col, row };
 };
